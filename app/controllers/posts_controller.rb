e594e694bc9c0295260790f3035a0b2037089b2a
iscoop <mkromah@hotmail.co.uk>
2014-07-26 03:47:52 +0800

    added title to user and did some long debug

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,6 +34,9 @@ class PostsController < ApplicationController
       end
   end
 
+def show
+end
+  
   # PATCH/PUT /updates a post
   
   def update
@@ -66,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:summary, :source)
+      params.require(:post).permit(:summary, :title,:source)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140722142512) do
+ActiveRecord::Schema.define(version: 20140725165119) do
 
   create_table "posts", force: true do |t|
     t.text     "summary"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140722142512) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "title"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
