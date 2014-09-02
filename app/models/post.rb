a139f879c3376770a0b2289175f2526fa9acc9be
iscoop <mkromah@hotmail.co.uk>
2014-09-02 13:56:05 +0100

    validation changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,8 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	
 	validates :title, presence: true
-	validates :summary, presence: true
-	validates :summary2, presence: true
-	validates :summary3, presence: true
-	validates :summary, length: {maximum: 90}
-	validates :summary2, length: {maximum: 90}
-	validates :summary3, length: {maximum: 90}
-	validates :source, presence: true
+	validates :title, length: {maximum: 300}
+	
+
+	
 end
